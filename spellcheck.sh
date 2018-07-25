b3e8a7ce505dd83578958249c842b96206a78c47
muthu-git <muthunakumar@gmail.com>
2018-07-25 19:39:29 +0530

    Update spellcheck.sh

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find . -iname "*".yml > list
-for i in `cat list`;
-do 
-  ansible-playbook --syntax-check $i 
-done
+#find . -iname "*".yml > list
+#for i in `cat list`;
+#do 
+ # ansible-playbook --syntax-check $i 
+#done
